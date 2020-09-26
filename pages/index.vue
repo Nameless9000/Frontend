4400dad71481c8f49914d89a8ca824ad04a43724
aspect <airogfx@gmail.com>
2020-09-25 22:33:00 -0400

    Update css.

--- pages/index.vue
+++ pages/index.vue
@@ -42,35 +42,23 @@
       </vs-button>
     </div>
 
-    <vs-dialog v-model="active">
+    <vs-dialog v-model="active" blur class="auth-box">
       <template #header>
-        <h4 class="not-margin">
+        <h4 class="header-text">
           Login
         </h4>
       </template>
 
       <div class="con-form">
-        <vs-input v-model="email" placeholder="Email">
-          <template #icon>
-            @
-          </template>
-        </vs-input>
-        <vs-input v-model="password" type="password" placeholder="Password">
-          <template #icon>
-            <i class="bx bxs-lock" />
-          </template>
-        </vs-input>
+        <vs-input v-model="email" placeholder="Email" />
+        <vs-input v-model="password" type="password" placeholder="Password" />
       </div>
 
       <template #footer>
         <div class="footer-dialog">
-          <vs-button block>
+          <vs-button block color="danger">
             Sign In
           </vs-button>
-
-          <div class="new">
-            New Here? <a href="#">Create New Account</a>
-          </div>
         </div>
       </template>
     </vs-dialog>
@@ -88,3 +76,48 @@ export default {
     },
 };
 </script>
+
+<style lang="stylus">
+    getColor(vsColor, alpha = 1)
+        unquote("rgba(var(--vs-"+vsColor+"), "+alpha+")")
+    getVar(var)
+        unquote("var(--vs-"+var+")")
+    .not-margin
+      margin 0px
+      font-weight normal
+      padding 10px
+    .con-form
+      width 100%
+      .flex
+        display flex
+        align-items center
+        justify-content space-between
+        a
+          font-size .8rem
+          opacity .7
+          &:hover
+            opacity 1
+      .vs-checkbox-label
+        font-size .8rem
+      .vs-input-content
+        margin 10px 0px
+        width calc(100%)
+        .vs-input
+          width 100%
+    .footer-dialog
+      display flex
+      align-items center
+      justify-content center
+      flex-direction column
+      width calc(100%)
+      .new
+        margin 0px
+        margin-top 20px
+        padding: 0px
+        font-size .7rem
+        a
+          color getColor('primary') !important
+          margin-left 6px
+          &:hover
+            text-decoration underline
+    </style>
