442ae485a61767fa24f1527f13bb2d502ba9bb7d
aspect <airogfx@gmail.com>
2020-09-28 11:20:53 -0400

    Update verify component

--- components/Verify.vue
+++ components/Verify.vue
@@ -1,15 +1,35 @@
 <template>
   <div class="header">
-    {{ bruh }}
+    <h3 class="verifyTitle">
+      Thank you for registering, just one last step!
+    </h3>
+    <p class="verifyCaption">
+      Please link your Discord account to Astral, this is to verify your identity & join the discord server.
+    </p>
+    <div class="verifyButtons">
+      <vs-button href="http://localhost:3000/auth/discord/link" class="button" size="large" color="danger">
+        Login with Discord
+      </vs-button>
+      <vs-button class="button" size="large" color="danger" @click="logout">
+        Logout
+      </vs-button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-    data () {
-        return {
-            bruh: 'lol',
-        };
+    methods: {
+        logout () {
+            this.$axios.get('http://localhost:3000/auth/logout', { withCredentials: true })
+                .then((res) => {
+                    if (res.data.success) {
+                        this.$router.push('/');
+                    }
+                }).catch(() => {
+                    this.$router.push('/');
+                });
+        },
     },
 };
 </script>
