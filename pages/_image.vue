43500cc8ad4c95edb9f53a97cd8696d16d8b02ad
aspect <airogfx@gmail.com>
2020-10-01 12:50:18 -0400

    Update _image.vue

--- pages/_image.vue
+++ pages/_image.vue
@@ -1,7 +1,8 @@
 <template>
-  <div>
-    <h1>
-      hi
+  <div style="display: flex; flex-direction: column; justify-content: center; align-items: center; margin-top: 250px">
+    <img :src="`http://localhost:3000/i/${file.file.file.name}`" alt="image">
+    <h1 style="color: white">
+      {{ file.file.file.originalName }} uploaded by {{ file.file.user }} on {{ file.file.file.date }}
     </h1>
   </div>
 </template>
@@ -10,11 +11,29 @@
 import Axios from 'axios';
 
 export default {
-    asyncData (context) {
-        Axios.get(`http://localhost:3000/files/${context.params.image}`)
-            .then((res) => {
-                console.log(res.data);
-            });
+    async asyncData (context) {
+        const res = await Axios.get(`http://localhost:3000/files/${context.params.image}`);
+        return {
+            file: res.data,
+        };
+    },
+    data () {
+        return {
+            file: null,
+        };
+    },
+    head () {
+        return {
+            title: this.file.file.file.originalName,
+            meta: [
+                // hid is used as unique identifier. Do not use `vmid` for it as it will not work
+                {
+                    hid: 'description',
+                    name: 'description',
+                    content: 'My custom description',
+                },
+            ],
+        };
     },
 };
 </script>
