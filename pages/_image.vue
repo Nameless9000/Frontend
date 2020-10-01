ff2b861440d52867cae26362bffc5daa37f76a7f
aspect <airogfx@gmail.com>
2020-10-01 14:06:23 -0400

    Update _image.vue

--- pages/_image.vue
+++ pages/_image.vue
@@ -1,9 +1,16 @@
 <template>
-  <div style="display: flex; flex-direction: column; justify-content: center; align-items: center; margin-top: 250px">
-    <img :src="`http://localhost:3000/i/${file.file.file.name}`" alt="image">
-    <h1 style="color: white">
-      {{ file.file.file.originalName }} uploaded by {{ file.file.user }} on {{ file.file.file.date }}
-    </h1>
+  <div>
+    <div v-if="file" style="display: flex; flex-direction: column; justify-content: center; align-items: center; margin-top: 250px">
+      <img :src="`http://localhost:3000/i/${file.file.file.name}`" alt="image">
+      <h1 style="color: white; font-size: 25px; margin-top: 20px">
+        {{ file.file.file.originalName }} uploaded by {{ file.file.user }} on {{ file.file.file.date }}
+      </h1>
+    </div>
+    <div v-else>
+      <h1 style="color: white">
+        Invalid file.
+      </h1>
+    </div>
   </div>
 </template>
 
@@ -13,9 +20,11 @@ import Axios from 'axios';
 export default {
     async asyncData (context) {
         const res = await Axios.get(`http://localhost:3000/files/${context.params.image}`);
-        return {
-            file: res.data,
-        };
+        if (res.data.success) {
+            return {
+                file: res.data,
+            };
+        }
     },
     data () {
         return {
@@ -23,17 +32,45 @@ export default {
         };
     },
     head () {
-        return {
-            title: this.file.file.file.originalName,
-            meta: [
-                // hid is used as unique identifier. Do not use `vmid` for it as it will not work
-                {
-                    hid: 'description',
-                    name: 'description',
-                    content: 'My custom description',
-                },
-            ],
-        };
+        if (this.file !== null) {
+            const tags = [];
+            tags.push('hi');
+            if (this.file.file.display.type === 'embed') {
+                tags.push(
+                    {
+                        hid: 'title',
+                        name: 'title',
+                        content: this.file.file.display.embed.title,
+                    },
+                    {
+                        hid: 'theme-color',
+                        name: 'theme-color',
+                        content: this.file.file.display.embed.color,
+                    },
+                    {
+                        hid: 'image',
+                        name: 'image',
+                        content: `https://astral.cool/i/${this.file.file.file.name}`,
+                    },
+                    {
+                        hid: 'card',
+                        name: 'card',
+                        content: 'summary_large_image',
+                    }
+                );
+                if (this.file.file.display.embed.text !== null && this.file.file.display.embed.text !== '') {
+                    tags.push({
+                        hid: 'description',
+                        name: 'description',
+                        content: this.file.file.display.embed.text,
+                    });
+                }
+            }
+            return {
+                title: this.file.file.file.name,
+                meta: tags,
+            };
+        }
     },
 };
 </script>
