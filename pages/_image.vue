079b5bb7eec6da530246d523836e4edb6995105b
aspect <airogfx@gmail.com>
2020-10-01 11:46:44 -0400

    Update _image.vue

---
+++ pages/_image.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>
+      hi
+    </h1>
+  </div>
+</template>
+
+<script>
+import Axios from 'axios';
+
+export default {
+    asyncData (context) {
+        Axios.get(`http://localhost:3000/files/${context.params.image}`)
+            .then((res) => {
+                console.log(res.data);
+            });
+    },
+};
+</script>
