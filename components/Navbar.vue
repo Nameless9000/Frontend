7b5d25e6945291833209a66c7cf665cf17f5db68
aspect <airogfx@gmail.com>
2020-10-06 16:43:36 -0400

    Update Navbar.vue

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,130 +1,48 @@
 <template>
-  <div>
-    <vs-navbar
-      center-collapsed
-      square
-      color="#151515"
-      class="navbar"
-      text-white
-    >
-      <template #left>
-        <img src="~/assets/logo.png" width="100px" alt="logo">
-        <vs-navbar-item
-          to="/"
-          :active="enabled === 'home'"
-        >
-          Home
-        </vs-navbar-item>
-        <vs-navbar-item
-          to="/domains"
-          :active="enabled === 'domains'"
-        >
-          Domains
-        </vs-navbar-item>
-        <vs-navbar-item
-          to="/dashboard"
-          :active="enabled === 'dashboard'"
-        >
-          Dashboard
-        </vs-navbar-item>
-      </template>
+  <vs-navbar
+    center-collapsed
+    square
+    color="#151515"
+    class="navbar"
+    text-white
+  >
+    <template #left>
+      <img src="~/assets/logo.png" width="100px" alt="logo">
+      <vs-navbar-item
+        to="/"
+        :active="enabled === 'home'"
+      >
+        Home
+      </vs-navbar-item>
+      <vs-navbar-item
+        to="/domains"
+        :active="enabled === 'domains'"
+      >
+        Domains
+      </vs-navbar-item>
+      <vs-navbar-item
+        to="/dashboard"
+        :active="enabled === 'dashboard'"
+      >
+        Dashboard
+      </vs-navbar-item>
+    </template>
 
-      <template #right>
-        <vs-button class="button">
-          <i class="bx bx-log-in" style="margin-right: 5px" /> Login
-        </vs-button>
-        <vs-button class="button">
-          <i class="bx bxs-group" style="margin-right: 5px" /> Register
-        </vs-button>
-        <vs-button
-          icon
-          class="phoneLogin"
-        >
-          <i class="bx bx-log-in" />
-        </vs-button>
-      </template>
-    </vs-navbar>
-
-    <vs-dialog v-model="active.login" blur class="auth-box">
-      <template #header>
-        <h4 class="header-text">
-          Login
-        </h4>
-      </template>
-
-      <vs-alert v-if="error !== ''" color="danger" class="error-box">
-        {{ error }}
-      </vs-alert>
-
-      <div class="con-form">
-        <vs-input
-          v-model="username"
-          type="text"
-          placeholder="Username"
-          :value="username !== '' ? username : ''"
-          @input="setInput($event, 'username')"
-        />
-        <vs-input
-          v-model="password"
-          type="password"
-          placeholder="Password"
-          :value="invite !== '' ? invite : ''"
-          @input="setInput($event, 'invite')"
-        />
-      </div>
-
-      <template #footer>
-        <div class="footer-dialog">
-          <vs-button block color="danger" class="dialog-button" @click="login">
-            Login
-          </vs-button>
-        </div>
-      </template>
-    </vs-dialog>
-
-    <vs-dialog v-model="active.register" blur class="auth-box">
-      <template #header>
-        <h4 class="header-text">
-          Register
-        </h4>
-      </template>
-
-      <vs-alert v-if="error !== ''" color="danger" class="error-box">
-        {{ error }}
-      </vs-alert>
-
-      <div class="con-form">
-        <vs-input
-          v-model="username"
-          type="text"
-          placeholder="Username"
-          :value="username !== '' ? username : ''"
-          @input="setInput($event, 'username')"
-        />
-        <vs-input
-          v-model="password"
-          type="password"
-          placeholder="Password"
-          :value="password !== '' ? password : ''"
-          @input="setInput($event, 'password')"
-        />
-        <vs-input
-          type="text"
-          placeholder="Invite Code"
-          :value="invite !== '' ? invite : ''"
-          @input="setInput($event, 'invite')"
-        />
-      </div>
-
-      <template #footer>
-        <div class="footer-dialog">
-          <vs-button block color="danger" class="dialog-button" @click="register">
-            Register
-          </vs-button>
-        </div>
-      </template>
-    </vs-dialog>
-  </div>
+    <template #right>
+      <vs-button class="button">
+        <i class="bx bx-log-in" style="margin-right: 5px" /> Login
+      </vs-button>
+      <vs-button class="button">
+        <i class="bx bxs-group" style="margin-right: 5px" /> Register
+      </vs-button>
+      <vs-button
+        icon
+        class="phoneLogin"
+      >
+        <i class="bx bx-log-in" />
+      </vs-button>
+    </template>
+  </vs-navbar>
 </template>
 
 <script>
