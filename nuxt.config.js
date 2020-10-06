2f5f44920b199a1bc4944c89f1f3a4127c30bfcc
aspect <airogfx@gmail.com>
2020-10-06 12:59:43 -0400

    Init new project

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,64 +1,68 @@
+import colors from 'vuetify/es5/util/colors'
+
 export default {
-    // Global page headers (https://go.nuxtjs.dev/config-head)
-    head: {
-        title: 'Astral',
-        meta: [
-            { charset: 'utf-8' },
-            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            {
-                hid: 'title',
-                name: 'title',
-                content: 'Astral, the superior image hosting service',
-            },
-            {
-                hid: 'description',
-                name: 'description',
-                content: 'Astral is a simple and powerful image hosting platform, with great support, competent developers, and a welcoming community.',
-            },
-            {
-                hid: 'theme-color',
-                name: 'theme-color',
-                content: '#e6394a',
-            },
-        ],
-        link: [
-            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-            { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/boxicons@2.0.5/css/boxicons.min.css' },
-        ],
-    },
-
-    // Global CSS (https://go.nuxtjs.dev/config-css)
-    css: [
-        'vuesax/dist/vuesax.css',
-        '~/assets/css/main.css',
-        '~/assets/css/dashboard.css',
-        '~/assets/css/profile.css',
+  // Global page headers (https://go.nuxtjs.dev/config-head)
+  head: {
+    titleTemplate: '%s - app',
+    title: 'app',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
     ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
 
-    // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-    plugins: [
-        '@/plugins/vuesax',
-    ],
+  // Global CSS (https://go.nuxtjs.dev/config-css)
+  css: [
+  ],
 
-    // Auto import components (https://go.nuxtjs.dev/config-components)
-    components: true,
+  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
+  plugins: [
+  ],
 
-    // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
-    buildModules: [
+  // Auto import components (https://go.nuxtjs.dev/config-components)
+  components: true,
+
+  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
+  buildModules: [
     // https://go.nuxtjs.dev/eslint
-        '@nuxtjs/eslint-module',
-    ],
+    '@nuxtjs/eslint-module',
+    // https://go.nuxtjs.dev/vuetify
+    '@nuxtjs/vuetify'
+  ],
 
-    // Modules (https://go.nuxtjs.dev/config-modules)
-    modules: [
+  // Modules (https://go.nuxtjs.dev/config-modules)
+  modules: [
     // https://go.nuxtjs.dev/axios
-        '@nuxtjs/axios',
-    ],
+    '@nuxtjs/axios'
+  ],
 
-    // Axios module configuration (https://go.nuxtjs.dev/config-axios)
-    axios: {},
+  // Axios module configuration (https://go.nuxtjs.dev/config-axios)
+  axios: {},
 
-    // Build Configuration (https://go.nuxtjs.dev/config-build)
-    build: {
-    },
-};
+  // Vuetify module configuration (https://go.nuxtjs.dev/config-vuetify)
+  vuetify: {
+    customVariables: ['~/assets/variables.scss'],
+    theme: {
+      dark: true,
+      themes: {
+        dark: {
+          primary: colors.blue.darken2,
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3
+        }
+      }
+    }
+  },
+
+  // Build Configuration (https://go.nuxtjs.dev/config-build)
+  build: {
+  }
+}
--- pages/index.vue
+++ pages/index.vue
@@ -1,247 +1,89 @@
 <template>
-  <div class="header">
-    <img
-      src="~/assets/logo.png"
-      alt="logo"
-      class="logo"
-    >
-
-    <p class="caption">
-      The superior image hosting service.
-    </p>
-
-    <div class="buttons">
-      <vs-button class="button" size="large" color="danger" @click="activate('login')">
-        Login
-      </vs-button>
-      <vs-button class="button" size="large" color="danger" @click="activate('register')">
-        Register
-      </vs-button>
-    </div>
-
-    <vs-dialog v-model="active.login" blur class="auth-box">
-      <template #header>
-        <h4 class="header-text">
-          Login
-        </h4>
-      </template>
-
-      <vs-alert v-if="error !== ''" color="danger" class="error-box">
-        {{ error }}
-      </vs-alert>
-
-      <div class="con-form">
-        <vs-input
-          v-model="username"
-          type="text"
-          placeholder="Username"
-          :value="username !== '' ? username : ''"
-          @input="setInput($event, 'username')"
-        />
-        <vs-input
-          v-model="password"
-          type="password"
-          placeholder="Password"
-          :value="invite !== '' ? invite : ''"
-          @input="setInput($event, 'invite')"
-        />
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <div class="text-center">
+        <logo />
+        <vuetify-logo />
       </div>
-
-      <template #footer>
-        <div class="footer-dialog">
-          <vs-button block color="danger" class="dialog-button" @click="login">
-            Login
-          </vs-button>
-        </div>
-      </template>
-    </vs-dialog>
-
-    <vs-dialog v-model="active.register" blur class="auth-box">
-      <template #header>
-        <h4 class="header-text">
-          Register
-        </h4>
-      </template>
-
-      <vs-alert v-if="error !== ''" color="danger" class="error-box">
-        {{ error }}
-      </vs-alert>
-
-      <div class="con-form">
-        <vs-input
-          v-model="username"
-          type="text"
-          placeholder="Username"
-          :value="username !== '' ? username : ''"
-          @input="setInput($event, 'username')"
-        />
-        <vs-input
-          v-model="password"
-          type="password"
-          placeholder="Password"
-          :value="password !== '' ? password : ''"
-          @input="setInput($event, 'password')"
-        />
-        <vs-input
-          type="text"
-          placeholder="Invite Code"
-          :value="invite !== '' ? invite : ''"
-          @input="setInput($event, 'invite')"
-        />
-      </div>
-
-      <template #footer>
-        <div class="footer-dialog">
-          <vs-button block color="danger" class="dialog-button" @click="register">
-            Register
-          </vs-button>
-        </div>
-      </template>
-    </vs-dialog>
-  </div>
+      <v-card>
+        <v-card-title class="headline">
+          Welcome to the Vuetify + Nuxt.js template
+        </v-card-title>
+        <v-card-text>
+          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
+          <p>
+            For more information on Vuetify, check out the <a
+              href="https://vuetifyjs.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              documentation
+            </a>.
+          </p>
+          <p>
+            If you have questions, please join the official <a
+              href="https://chat.vuetifyjs.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+              title="chat"
+            >
+              discord
+            </a>.
+          </p>
+          <p>
+            Find a bug? Report it on the github <a
+              href="https://github.com/vuetifyjs/vuetify/issues"
+              target="_blank"
+              rel="noopener noreferrer"
+              title="contribute"
+            >
+              issue board
+            </a>.
+          </p>
+          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
+          <div class="text-xs-right">
+            <em><small>&mdash; John Leider</small></em>
+          </div>
+          <hr class="my-3">
+          <a
+            href="https://nuxtjs.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Nuxt Documentation
+          </a>
+          <br>
+          <a
+            href="https://github.com/nuxt/nuxt.js"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Nuxt GitHub
+          </a>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn
+            color="primary"
+            nuxt
+            to="/inspire"
+          >
+            Continue
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
-export default {
-    asyncData (context) {
-        const code = context.query.code;
-        if (code) {
-            return {
-                active: {
-                    login: false,
-                    register: true,
-                },
-                invite: code,
-            };
-        }
-    },
-    data () {
-        return {
-            username: '',
-            password: '',
-            invite: '',
-            error: '',
-            active: {
-                login: false,
-                register: false,
-            },
-        };
-    },
-    created () {
-        this.$axios.get('http://localhost:3000/users/@me', { withCredentials: true })
-            .then((res) => {
-                const spinner = this.$vs.loading({
-                    background: '#050506',
-                });
-                if (res.data) {
-                    setTimeout(() => {
-                        spinner.close();
-                        this.$router.push('/dashboard');
-                    }, 800);
-                } else {
-                    setTimeout(() => {
-                        spinner.close();
-                    }, 800);
-                }
-            }).catch(() => {});
-    },
-    methods: {
-        activate (property) {
-            this.error = '';
-            this.active[property] = true;
-        },
-        setInput (val, property) {
-            this[property] = val;
-        },
-        resetForm () {
-            this.username = '';
-            this.password = '';
-            this.invite = '';
-        },
-        async login () {
-            if (this.username.length <= 0 || this.password.length <= 0) {
-                this.resetForm();
-                this.error = 'Please fill out all the fields.';
-            } else {
-                const res = await this.$axios.post('http://localhost:3000/auth/login', {
-                    username: this.username,
-                    password: this.password,
-                }, {
-                    withCredentials: true,
-                });
-                if (res.data.success) {
-                    window.location.href = 'http://localhost:3001/dashboard';
-                } else {
-                    this.resetForm();
-                    this.error = res.data.message;
-                }
-            }
-        },
-        async register () {
-            if (this.username.length <= 0 || this.password.length <= 0 || this.invite.length <= 0) {
-                this.resetForm();
-                this.error = 'Please fill out all the fields.';
-            } else {
-                const res = await this.$axios.post('http://localhost:3000/auth/register', {
-                    username: this.username,
-                    password: this.password,
-                    invite: this.invite,
-                }, {
-                    withCredentials: true,
-                });
-                if (res.data.success) {
-                    window.location.href = 'http://localhost:3001/dashboard';
-                } else {
-                    this.resetForm();
-                    this.error = res.data.message;
-                }
-            }
-        },
-    },
-};
-</script>
+import Logo from '~/components/Logo.vue'
+import VuetifyLogo from '~/components/VuetifyLogo.vue'
 
-<style lang="stylus">
-  getColor(vsColor, alpha = 1)
-      unquote("rgba(var(--vs-"+vsColor+"), "+alpha+")")
-  getVar(var)
-      unquote("var(--vs-"+var+")")
-  .not-margin
-    margin 0px
-    font-weight normal
-    padding 10px
-  .con-form
-    width 100%
-    .flex
-      display flex
-      align-items center
-      justify-content space-between
-      a
-        font-size .8rem
-        opacity .7
-        &:hover
-          opacity 1
-    .vs-checkbox-label
-      font-size .8rem
-    .vs-input-content
-      margin 5px 0px
-      width calc(100%)
-      .vs-input
-        width 100%
-  .footer-dialog
-    display flex
-    align-items center
-    justify-content center
-    flex-direction column
-    width calc(100%)
-    .new
-      margin 0px
-      margin-top 21px
-      padding: 0px
-      font-size .7rem
-      a
-        color getColor('primary') !important
-        margin-left 6px
-        &:hover
-          text-decoration underline
-</style>
+export default {
+  components: {
+    Logo,
+    VuetifyLogo
+  }
+}
+</script>
