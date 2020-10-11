5f9a9d9de33a0171cc55a73bb48fa4e2fd7f90a2
aspect <airogfx@gmail.com>
2020-10-10 20:39:47 -0400

    Update dashboard component

---
+++ components/Dashboard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    <h1>
+      {{ user.user }}
+    </h1>
+  </div>
+</template>
+
+<script>
+// import { Chrome } from 'vue-color';
+
+export default {
+    name: 'Dashboard',
+    // components: {
+    //     Chrome
+    // },
+    props: {
+        user: {
+            type: Object,
+            default () {
+                return {}
+            }
+        }
+    }
+}
+</script>
+
+<style>
+.container {
+
+}
+</style>
