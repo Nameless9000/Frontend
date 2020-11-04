dda873a018cb7c960bbc38d8f014f1003af9aada
aspect <airogfx@gmail.com>
2020-11-03 20:34:12 -0500

    Finish index and add dark theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
+import '../styles/antd.less';
 import '../styles/globals.css';
-import 'antd/dist/antd.css';
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,15 @@
+/* eslint-disable no-unused-vars */
 import { useState } from 'react';
-import { Form, Input, Button, Modal } from 'antd';
-import Head from 'next/head';
+import { Form, Input, Button, Modal, Tabs } from 'antd';
 import styles from '../styles/main.module.css';
+import Head from 'next/head';
+import { UserOutlined, LockOutlined, MailOutlined, CheckOutlined } from '@ant-design/icons';
 import Axios from 'axios';
+const { TabPane } = Tabs;
+import { SiDiscord } from 'react-icons/si';
 
 const initialState = {
-  showLogin: false,
-  showRegister: false,
+  showModal: false,
   username: '',
   password: '',
   email: '',
@@ -14,22 +17,18 @@ const initialState = {
 };
 
 export default function Home() {
-  const [{ showLogin, showRegister, username, password, email, invite }, setState] = useState(initialState);
+  const [{ showModal, username, password, email, invite }, setState] = useState(initialState);
 
   const toggleModal = () => {
-    setState((state) => ({ ...state, showLogin: true }));
+    setState((state) => ({ ...state, showModal: true }));
   };
 
   const closeModal = () => {
-    setState((state) => ({ ...state, showLogin: false }));
+    setState((state) => ({ ...state, showModal: false }));
   };
 
   const setInput = (property: string, val: string) => {
     setState((state) => ({ ...state, [property]: val }));
-  };
-
-  const resetForm = () => {
-    setState({ ...initialState });
   };
 
   const login = async () => {
@@ -56,43 +55,89 @@ export default function Home() {
         <h1 style={{ color: 'white' }}>astral.cool</h1>
         <p style={{ fontSize: '20px', marginTop: '-20px' }}>private invite-only image host.</p>
         <div style={{ marginTop: '-10px' }}>
-          <Button style={{ marginRight: '5px' }} onClick={toggleModal}>Login</Button>
-          <Button>Register</Button>
+          <Button style={{ marginRight: '5px' }} onClick={toggleModal}>Login/Register</Button>
         </div>
 
         <Modal
-          title="Login"
-          visible={showLogin}
+          title={<Tabs defaultActiveKey="1" centered type="card">
+            <TabPane tab="Login" key="1">
+              <Form
+                name="basic"
+                initialValues={{ remember: true }}
+                style={{ marginTop: '10px' }}
+              >
+                <Form.Item
+                  name="username"
+                  rules={[{ required: true, message: 'Provide a valid username' }]}
+                >
+                  <Input size="large" placeholder="Username" prefix={<UserOutlined />} />
+                </Form.Item>
+
+                <Form.Item
+                  name="password"
+                  rules={[{ required: true, message: 'Provide a valid password', min: 5 }]}
+                >
+                  <Input.Password size="large" placeholder="Password" prefix={<LockOutlined />} />
+                </Form.Item>
+
+                <Form.Item>
+                  <Button block size="large">
+                    Login
+                  </Button>
+                  <Button icon={<SiDiscord style={{ marginRight: '8px' }} />} type="primary" block size="large" style={{ marginTop: '10px', marginBottom: '-35px', backgroundColor: '#7289DA', border: 'none' }}>
+                    Login with Discord
+                  </Button>
+                </Form.Item>
+              </Form>
+            </TabPane>
+            <TabPane tab="Register" key="2">
+              <Form
+                name="basic"
+                initialValues={{ remember: true }}
+                style={{ marginTop: '10px' }}
+              >
+                <Form.Item
+                  name="username"
+                  rules={[{ required: true, message: 'Provide a valid username' }]}
+                >
+                  <Input size="large" placeholder="Username" prefix={<UserOutlined />} />
+                </Form.Item>
+
+                <Form.Item
+                  name="password"
+                  rules={[{ required: true, message: 'Provide a valid password', min: 5 }]}
+                >
+                  <Input.Password size="large" placeholder="Password" prefix={<LockOutlined />} />
+                </Form.Item>
+
+                <Form.Item
+                  name="email"
+                  rules={[{ required: true, message: 'Provide a valid email', type: 'email' }]}
+                >
+                  <Input size="large" placeholder="Email" prefix={<MailOutlined />} />
+                </Form.Item>
+
+                <Form.Item
+                  name="invite"
+                  rules={[{ required: true, message: 'Provide a valid invite' }]}
+                >
+                  <Input size="large" placeholder="Invite" prefix={<CheckOutlined />} />
+                </Form.Item>
+
+                <Form.Item>
+                  <Button block size="large" style={{ marginBottom: '-30px' }}>
+                    Register
+                  </Button>
+                </Form.Item>
+              </Form>
+            </TabPane>
+          </Tabs>}
+          visible={showModal}
           centered
           onCancel={closeModal}
           footer={null}
         >
-          <Form
-            name="basic"
-            initialValues={{ remember: true }}
-          >
-            <Form.Item
-              label="Username"
-              name="username"
-              rules={[{ required: true, message: 'Please provide a valid username.' }]}
-            >
-              <Input onChange={(val) => setInput('username', val.target.value)} />
-            </Form.Item>
 
-            <Form.Item
-              label="Password"
-              name="password"
-              rules={[{ required: true, message: 'Please provide a valid password.', min: 5 }]}
-            >
-              <Input.Password onChange={(val) => setInput('password', val.target.value)} />
-            </Form.Item>
-
-            <Form.Item style={{ marginBottom: '0px', marginTop: '10px' }}>
-              <Button type="ghost" block onClick={login}>
-                Login
-              </Button>
-            </Form.Item>
-          </Form>
         </Modal>
       </main>
     </div>
