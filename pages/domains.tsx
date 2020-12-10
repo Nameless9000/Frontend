ab0f81b15af69a36566d7fb1f516b6aff02ce225
aspect <airogfx@gmail.com>
2020-12-10 18:43:24 -0500

    Update domains page to be simpler

--- pages/domains.tsx
+++ pages/domains.tsx
@@ -2,55 +2,41 @@ import React, { useEffect, useState } from 'react';
 import Axios from 'axios';
 import styles from '../styles/Domains.module.css';
 import Link from 'next/link';
+import { Button } from 'antd';
 
 export default function Domains() {
     const [domains, setDomains] = useState([]);
 
     useEffect(() => {
-        Axios.get(`${process.env.BACKEND_URL}/domains`)
-            .then(({ data }) => {
-                for (const domain of data.domains) {
-                    domain.name = domain.name.replace('nigga', 'n*gga')
-                        .replace('nigger', 'n*gger');
-                }
+        Axios.get(`${process.env.BACKEND_URL}/domains`).then(({ data }) => {
+            for (const domain of data.domains) {
+                domain.name = domain.name
+                    .replace('nigga', 'n*gga')
+                    .replace('nigger', 'n*gger');
+            }
 
-                setDomains(data.domains);
-            });
+            setDomains(data.domains);
+        });
     }, []);
 
     return (
         <div className={styles.container}>
-            <Link href="/">
-                <p style={{
-                    cursor: 'pointer',
-                    marginTop: '10px',
-                }}>
-                      Back
-                </p>
-            </Link>
-
-            <h2 style={{
-                marginTop: '-5px',
-                marginLeft: '5px',
-                fontWeight: 400,
-            }}>There are <strong>{domains.length}</strong> domains.</h2>
-
-            <h3 className={styles.title}>Wildcard ({domains.filter((d) => d.wildcard).length})</h3>
+            <h3 style={{ marginTop: '10px' }}>Wildcard ({domains.filter((d) => d.wildcard).length})</h3>
             <ul>
-                {domains.filter((d) => d.wildcard).map((d) => (
-                    <li key={d.name}>
-                        {d.name}
-                    </li>
-                ))}
+                {domains
+                    .filter((d) => d.wildcard)
+                    .map((d) => (
+                        <li key={d.name}>{d.name}</li>
+                    ))}
             </ul>
 
-            <h3 className={styles.title}>Non-Wildcard ({domains.filter((d) => !d.wildcard).length})</h3>
+            <h3>Non-Wildcard ({domains.filter((d) => !d.wildcard).length})</h3>
             <ul>
-                {domains.filter((d) => !d.wildcard).map((d) => (
-                    <li key={d.name}>
-                        {d.name}
-                    </li>
-                ))}
+                {domains
+                    .filter((d) => !d.wildcard)
+                    .map((d) => (
+                        <li key={d.name}>{d.name}</li>
+                    ))}
             </ul>
         </div>
     );
